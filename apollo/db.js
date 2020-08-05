ca64767bc60b31b7aaf4a81996a4407187676b40
jeshio <jeshio@yandex.ru>
2020-08-05 09:09:40 +0500

    2:apollo: добавляет eslint и корневой пакет

--- apollo/db.js
+++ apollo/db.js
@@ -1,25 +1,25 @@
-const mongoose = require('mongoose');
+import { connect } from 'mongoose'
 
 const {
   MONGO_USERNAME,
   MONGO_PASSWORD,
   MONGO_HOSTNAME,
   MONGO_PORT,
-  MONGO_DB
-} = process.env;
+  MONGO_DB,
+} = process.env
 
 const options = {
   useNewUrlParser: true,
   reconnectTries: Number.MAX_VALUE,
   reconnectInterval: 500,
   connectTimeoutMS: 10000,
-};
+}
 
-const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`
 
-mongoose.connect(url, options).then( function() {
-  console.log('MongoDB is connected!');
+connect(url, options).then(() => {
+  console.log('MongoDB is connected!')
 })
-  .catch( function(err) {
-  console.log(err);
-});
+  .catch((err) => {
+    console.log(err)
+  })
--- apollo/index.js
+++ apollo/index.js
@@ -1,28 +1,26 @@
-import express from "express";
-import cors from 'cors';
-import { ApolloServer, gql } from 'apollo-server-express';
-import './db';
+import express from 'express'
+import cors from 'cors'
+import { ApolloServer, gql } from 'apollo-server-express'
+import './db'
 
 const typeDefs = gql`
     type Query {
         hello: String
     }
-`;
+`
 
 const resolvers = {
-    Query: {
-        hello: () => 'Hello World!!'
-    }
-};
+  Query: {
+    hello: () => 'Hello World!!',
+  },
+}
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers })
 
-const app = express();
+const app = express()
 
-app.use(cors());
+app.use(cors())
 
-server.applyMiddleware({ app });
+server.applyMiddleware({ app })
 
-app.listen({ port: 4000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath} !`)
-);
+app.listen({ port: 4000 }, () => console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath} !`))
